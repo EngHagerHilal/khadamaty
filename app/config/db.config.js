b13ea0b5429b9e141debe1df68b145ef0718c070
EngHagerHilal <61092548+EngHagerHilal@users.noreply.github.com>
2020-03-25 21:47:12 +0200

    Update db.config.js

--- app/config/db.config.js
+++ app/config/db.config.js
@@ -1,8 +1,6 @@
 module.exports = {
-    HOST: "localhost",
-    USER: "root",
-    PASSWORD: "",
-    DB: "khadamaty",
-    dialect: "mysql"
+  HOST: "us-cdbr-iron-east-01.cleardb.net",
+  USER: "b51eba763801ee",
+  PASSWORD: "37fdc5dd",
+  DB: "heroku_c857664073e7ab5"
 };
-  
