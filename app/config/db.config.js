7c716da8dcef482e8cc95d3a1c43772f88c34426
Hager Hilal <Hagerhilal2@gmail.com>
2020-03-26 01:31:48 +0200

    local database

--- app/config/db.config.js
+++ app/config/db.config.js
@@ -1,8 +1,8 @@
 module.exports = {
-    HOST: "us-cdbr-iron-east-01.cleardb.net",
-    USER: "b51eba763801ee",
-    PASSWORD: "37fdc5dd",
-    DB: "heroku_c857664073e7ab5",
+    HOST: "localhost",
+    USER: "root",
+    PASSWORD: "",
+    DB: "khadamaty",
     dialect: "mysql"
 };
   
